df1446d7877f99d1016eaf0400745da906d8038a
Jon Jagger <jon@jaggersoft.com>
2020-02-13 10:45:42 +0000

    Pass coverage group names as env-vars; detect simplecov version automatically

--- app/check_test_results.rb
+++ app/check_test_results.rb
@@ -4,15 +4,49 @@
 # 1) The simplecov coverage index.html file.
 #    It would be nice if simplecov saved the raw data to a json file
 #    and created the html from that, but alas it does not.
-#    At the moment its from simplecov 0.17.1
+#    At the moment its from simplecov 0.17.0
 #    Simplecov will soon support branch-coverage.
-#    There is some preparatory work for that in the differ
-#    repo (branch == server-branch-coverage)
-
-TEST_LOG=ARGV[0]   # eg /app/data/test.log
-INDEX_HTML=ARGV[1] # eg /app/data/index.html
 
 require_relative 'metrics'
+
+# - - - - - - - - - - - - - - - - - - - - - - -
+def fatal_error(message)
+  puts message
+  exit(42)
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - -
+def coverage_code_group_name
+  ENV['COVERAGE_CODE_GROUP_NAME']
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - -
+def coverage_test_group_name
+  ENV['COVERAGE_TEST_GROUP_NAME']
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - -
+def test_log
+  $test_log ||= begin
+    path = ARGV[0] # eg /app/data/test.log
+    cleaned(`cat #{path}`)
+  end
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - -
+def index_html
+  $index_html = begin
+    path = ARGV[1] # eg /app/data/index.html
+    cleaned(`cat #{path}`)
+  end
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - -
+def cleaned(s)
+  # guard against invalid byte sequence
+  s = s.encode('UTF-16', 'UTF-8', :invalid => :replace, :replace => '')
+  s = s.encode('UTF-8', 'UTF-16')
+end
 
 # - - - - - - - - - - - - - - - - - - - - - - -
 def number
@@ -24,13 +58,6 @@ def f2(s)
   result = ("%.2f" % s).to_s
   result += '0' if result.end_with?('.0')
   result
-end
-
-# - - - - - - - - - - - - - - - - - - - - - - -
-def cleaned(s)
-  # guard against invalid byte sequence
-  s = s.encode('UTF-16', 'UTF-8', :invalid => :replace, :replace => '')
-  s = s.encode('UTF-8', 'UTF-16')
 end
 
 # - - - - - - - - - - - - - - - - - - - - - - -
@@ -49,14 +76,45 @@ end
 def get_index_stats(name)
   # It would be nice if simplecov saved the raw data to a json file
   # and created the html from that, but alas it does not.
-  get_index_stats_gem_0_17_0(name)
-  #get_index_stats_gem_0_18_1(name)
+  if index_html.include?('v0.17.0')
+    return get_index_stats_gem_0_17_0(name)
+  end
+  if index_html.include?('v0.18.1')
+    return get_index_stats_gem_0_18_1(name)
+  end
+  fatal_error('Unknown simplecov version')
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - -
+def get_index_stats_gem_0_17_0(name)
+  pattern = /<div class=\"file_list_container\" id=\"#{name}\">
+  \s*<h2>\s*<span class=\"group_name\">#{name}<\/span>
+  \s*\(<span class=\"covered_percent\"><span class=\"\w+\">([\d\.]*)\%<\/span><\/span>
+  \s*covered at
+  \s*<span class=\"covered_strength\">
+  \s*<span class=\"\w+\">
+  \s*(#{number})
+  \s*<\/span>
+  \s*<\/span> hits\/line\)
+  \s*<\/h2>
+  \s*<a name=\"#{name}\"><\/a>
+  \s*<div>
+  \s*<b>#{number}<\/b> files in total.
+  \s*<b>(#{number})<\/b> relevant lines./m
+
+  r = index_html.match(pattern)
+  fatal_error('REGEX match failed...') if r.nil?
+
+  h = {}
+  h[:coverage]      = f2(r[1])
+  h[:hits_per_line] = f2(r[2])
+  h[:line_count]    = r[3].to_i
+  h[:name] = name
+  h
 end
 
 # - - - - - - - - - - - - - - - - - - - - - - -
 def get_index_stats_gem_0_18_1(name)
-  html = `cat #{ARGV[1]}`
-  html = cleaned(html)
   pattern = /<div class=\"file_list_container\" id=\"#{name}\">
   \s*<h2>\s*<span class=\"group_name\">#{name}<\/span>
   \s*\(<span class=\"covered_percent\">
@@ -77,45 +135,8 @@ def get_index_stats_gem_0_18_1(name)
   \s*<div class=\"t-line-summary\">\s*
   \s*<b>(#{number})<\/b> relevant lines./m
 
-  r = html.match(pattern)
-
-  if r.nil?
-    puts "REGEX match failed..."
-    exit(42)
-  end
-
-  h = {}
-  h[:coverage]      = f2(r[1])
-  h[:hits_per_line] = f2(r[2])
-  h[:line_count]    = r[3].to_i
-  h[:name] = name
-  h
-end
-
-# - - - - - - - - - - - - - - - - - - - - - - -
-def get_index_stats_gem_0_17_0(name)
-  html = `cat #{INDEX_HTML}`
-  html = cleaned(html)
-  pattern = /<div class=\"file_list_container\" id=\"#{name}\">
-  \s*<h2>\s*<span class=\"group_name\">#{name}<\/span>
-  \s*\(<span class=\"covered_percent\"><span class=\"\w+\">([\d\.]*)\%<\/span><\/span>
-  \s*covered at
-  \s*<span class=\"covered_strength\">
-  \s*<span class=\"\w+\">
-  \s*(#{number})
-  \s*<\/span>
-  \s*<\/span> hits\/line\)
-  \s*<\/h2>
-  \s*<a name=\"#{name}\"><\/a>
-  \s*<div>
-  \s*<b>#{number}<\/b> files in total.
-  \s*<b>(#{number})<\/b> relevant lines./m
-  r = html.match(pattern)
-
-  if r.nil?
-    puts "REGEX match failed..."
-    exit(42)
-  end
+  r = index_html.match(pattern)
+  fatal_error('REGEX match failed...') if r.nil?
 
   h = {}
   h[:coverage]      = f2(r[1])
@@ -127,9 +148,6 @@ end
 
 # - - - - - - - - - - - - - - - - - - - - - - -
 def get_test_log_stats
-  test_log = `cat #{TEST_LOG}`
-  test_log = cleaned(test_log)
-
   stats = {}
 
   warning_regex = /: warning:/m
@@ -140,7 +158,10 @@ def get_test_log_stats
   stats[:time]               = f2(m[1])
   stats[:tests_per_sec]      = m[2].to_i
 
-  summary_pattern = %w(runs assertions failures errors skips).map{ |s| "(#{number}) #{s}" }.join(', ')
+  summary_pattern =
+    %w(runs assertions failures errors skips)
+    .map{ |s| "(#{number}) #{s}" }
+    .join(', ')
   m = test_log.match(Regexp.new(summary_pattern))
   stats[:test_count]      = m[1].to_i
   stats[:assertion_count] = m[2].to_i
@@ -153,8 +174,8 @@ end
 
 # - - - - - - - - - - - - - - - - - - - - - - -
 log_stats = get_test_log_stats
-test_stats = get_index_stats('test')
-app_stats = get_index_stats('app')
+test_stats = get_index_stats(coverage_test_group_name)
+app_stats = get_index_stats(coverage_code_group_name)
 
 # - - - - - - - - - - - - - - - - - - - - - - -
 test_count    = log_stats[:test_count]
@@ -168,6 +189,10 @@ app_coverage  = app_stats[:coverage].to_f
 test_coverage = test_stats[:coverage].to_f
 
 line_ratio = (test_stats[:line_count].to_f / app_stats[:line_count].to_f)
+
+#puts " app_stats[:hits_per_line] == #{app_stats[:hits_per_line].to_f}"
+#puts "test_stats[:hits_per_line] == #{test_stats[:hits_per_line].to_f}"
+
 hits_ratio = (app_stats[:hits_per_line].to_f / test_stats[:hits_per_line].to_f)
 
 table = [
